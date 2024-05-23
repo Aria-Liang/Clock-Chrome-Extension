0e6ba57fb61fea0c49557b981ae62d4b60bea230
Erik Gao <erikgao@Coding-Machine.local>
2024-05-22 23:09:03 -0400

    check condition of prompt

--- content.js
+++ content.js
@@ -2,10 +2,13 @@ let name = prompt('Hi, what is your name?');
 let timeInput = prompt(
   'How often would you like to be reminded to take a break?'
 );
-let time = Number(timeInput * 1000);
 
 const alertUser = () => {
   alert(`Hi, ${name}! It's time to take a break!`);
 };
 
+while (isNaN(timeInput)) {
+  timeInput = prompt('Please input a valid number');
+}
+let time = Number(timeInput * 1000);
 setTimeout(alertUser, time)
